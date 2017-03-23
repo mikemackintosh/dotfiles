4a0cb341f5d409303a1c3bb75f565f24f6fb6ace
Mike Mackintosh <mike.mackintosh@snapchat.com>
2017-03-23 09:02:28 -0700

    fix the chrome extractor

--- bash/chrome.sh
+++ bash/chrome.sh
@@ -4,5 +4,5 @@ function dlext() {
   mkdir -p /tmp/extension/
   curl -L -o "/tmp/extension/$extension_id.zip" "https://clients2.google.com/service/update2/crx?response=redirect&os=mac&arch=x86-64&nacl_arch=x86-64&prod=chromecrx&prodchannel=stable&prodversion=44.0.2403.130&x=id%3D$extension_id%26uc"
   unzip -d "/tmp/extension/$extension_id-source" "/tmp/extension/$extension_id.zip"
-  cd /tmp/extension/$extension_id
+  cd /tmp/extension/$extension_id-source
 }
