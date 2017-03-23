07154fbf5ff36183bb3aecb603448d3aaa6e8ff2
Mike Mackintosh <mike.mackintosh@snapchat.com>
2017-03-23 09:01:59 -0700

    add crlhelper

---
+++ bash/crl.sh
@@ -0,0 +1,5 @@
+function getcrl() {
+  CRL=$(openssl s_client -connect $1:443 < /dev/null 2>/dev/null| openssl x509 -text -noout 2>/dev/null | grep crl | sed -e 's/^.*URI:\(.*\)/\1/')
+  wget -O crl.der $CRL
+  openssl crl -inform der -in crl.der -noout -nameopt oneline,-esc_msb -text
+}
