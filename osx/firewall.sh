1c54f0f38bfd1bc530884f806fcd8115a8c76904
Mike Mackintosh <m@zyp.io>
2018-09-03 15:45:38 -0700

    add in the osx firewall script

---
+++ osx/firewall.sh
@@ -0,0 +1,2 @@
+/usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
+/usr/libexec/ApplicationFirewall/socketfilterfw --setloggingmode on/usr/libexec/ApplicationFirewall/socketfilterfw --setstealthmode on
