5e12588d3cadeba70556bdd0660fda88aa0ccd98
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2019-02-01 08:51:25 -0800

    Update gcloud.sh

--- bash/gcloud.sh
+++ bash/gcloud.sh
@@ -3,3 +3,6 @@ if [ -f '/usr/local/gcloud/path.bash.inc' ]; then source '/usr/local/gcloud/path
 
 # The next line enables shell command completion for gcloud.
 if [ -f '/usr/local/gcloud/completion.bash.inc' ]; then source '/usr/local/gcloud/completion.bash.inc'; fi
+
+# Add appengine alias
+alias appengine="/usr/local/google-cloud-sdk/bin/dev_appserver.py"
