c2a3e754b18c31a5bcf9183414cb761f52d12272
Mike Mackintosh <mikem@snap.com>
2019-11-11 10:16:36 -0800

    updates

--- bash/alias.sh
+++ bash/alias.sh
@@ -43,3 +43,4 @@ function pacman() {
   echo "Wakka, Wakka, Wakka..."
   echo -e "\n\033[1;33mᗧ\033[0m • • • \033[1;31mᗣ\033[0m  • •\n"
 }
+alias fstat="stat -x"
--- bash/ssh.sh
+++ bash/ssh.sh
@@ -1,22 +1,24 @@
 SSH_ENV=$HOME/.ssh/environment
 
 function start_agent {
-     echo "Initialising new SSH agent..."
-     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > ${SSH_ENV}
-     echo succeeded
-     chmod 600 ${SSH_ENV}
-     . ${SSH_ENV} > /dev/null
-     /usr/bin/ssh-add;
+    if [ -f "${SSH_ENV}" ]; then
+       . ${SSH_ENV} > /dev/null
+       #ps ${SSH_AGENT_PID} doesn't work under cywgin
+       (ps -efp ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null && echo "Using existing ssh-agent") || {
+         new_ssh_agent
+       }
+   else
+      new_ssh_agent
+   fi
 }
 
-# Source SSH settings, if applicable
+function new_ssh_agent {
+  echo "Initialising new SSH agent..."
+  /usr/bin/ssh-agent | sed 's/^echo/#echo/' >| ${SSH_ENV}
+  echo succeeded
+  chmod 600 ${SSH_ENV}
+  . ${SSH_ENV} > /dev/null
+  /usr/bin/ssh-add;
+}
 
-if [ -f "${SSH_ENV}" ]; then
-     . ${SSH_ENV} > /dev/null
-     #ps ${SSH_AGENT_PID} doesn't work under cywgin
-     ps -efp ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-         start_agent;
-     }
-else
-     start_agent;
-fi
+start_agent
