10ab9665ae72bec91e85f10e9554558fe5fed77b
Mike Mackintosh <m@zyp.io>
2022-07-16 19:22:13 -0700

    add qmk updates

--- qmk/duppster/keymap.c
+++ qmk/duppster/keymap.c
@@ -2,6 +2,12 @@
 
 keymap_config_t keymap_config;
 
+bool is_alt_tab_active = false; //
+uint16_t alt_tab_timer = 0;     //
+
+bool is_alt_tilde_active = false; //
+uint16_t alt_tilde_timer = 0;     //
+
 static const char * cmds[] = {
     "git init ",
     "git clone ",
@@ -11,17 +17,17 @@ static const char * cmds[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_65_ansi_blocker(
-    KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-    KC_GESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  \
+    ALT_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
+    KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
   [1] = LAYOUT_65_ansi_blocker(
     KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
     _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
     KC_LSCR, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, Z_LS,    _______, _______,          _______, KC_BRIU, \
-    _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, LAG_SWP, CHNG_MD, _______,          KC_VOLU, KC_BRID, \
+    _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, LAG_SWP,   Z_PIN,   Z_EDB,          KC_VOLU, KC_BRID, \
     _______, _______, _______,                            KC_MPLY,                            _______, _______, KC_MRWD, KC_VOLD, KC_MFFD  \
   ),
   /*
@@ -31,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
     _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
-  ),
+  // ),
   */
 };
 
@@ -42,8 +48,21 @@ void matrix_init_user(void) {
 };
 
 // Runs constantly in the background, in a loop.
-void matrix_scan_user(void) {
-};
+void matrix_scan_user(void) { // The very important timer.
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
+
+  if (is_alt_tilde_active) {
+    if (timer_elapsed(alt_tilde_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tilde_active = false;
+    }
+  }
+}
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
@@ -54,11 +73,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint32_t key_timer;
 
     switch (keycode) {
-        case KC_S:
-            if (record->event.pressed && MODS_RALT) {
-              send_string_with_delay(cmds[G_STATUS - G_INIT], 1);
-            }
-            return false;
         case CHNG_MD:
             if (record->event.pressed) {
                 dprintf("change_mode pressed\r\n");
@@ -103,6 +117,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+
         case RGB_TOG:
             if (record->event.pressed) {
               switch (rgb_matrix_get_flags()) {
@@ -129,6 +144,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               }
             }
             return false;
+
+        case ALT_TAB:
+          if (record->event.pressed) {
+            if (!is_alt_tab_active) {
+              is_alt_tab_active = true;
+              register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+          } else {
+            unregister_code(KC_TAB);
+          }
+          return false;
+
+       case ALT_TILDE:
+          if (record->event.pressed) {
+            if (!is_alt_tilde_active) {
+              is_alt_tilde_active = true;
+              register_code(KC_LALT);
+            }
+            alt_tilde_timer = timer_read();
+            register_code(KC_GRV);
+          } else {
+            unregister_code(KC_GRV);
+          }
+          return false;
+
         case G_INIT ... Z_LS:
             if (record->event.pressed) {
                 send_string_with_delay(cmds[keycode - G_INIT], 1);
--- qmk/duppster/keymap.h
+++ qmk/duppster/keymap.h
@@ -3,6 +3,7 @@
 #include <print.h>
 #include <string.h>
 
+
 enum alt_keycodes {
   U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
   U_T_AGCR,              //USB Toggle Automatic GCR control
@@ -15,10 +16,17 @@ enum alt_keycodes {
   TERMINAL,              //
 };
 
+enum custom_keycodes {          // Make sure have the awesome keycode ready
+  ALT_TAB = TERMINAL + 1,
+  ALT_TILDE,
+};
+
 enum string_macro_keycodes {
   // The start of this enum should always be equal to end of alt_keycodes + 1
-  G_INIT = TERMINAL + 1, // git init
+  G_INIT = ALT_TILDE + 1, // git init
   G_CLONE,               // git clone
   G_STATUS,              // git status
   Z_LS,                  // ls -la
+  Z_PIN, //pin
+  Z_EDB, // edb
 };
