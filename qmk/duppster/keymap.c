e4247a0776c3d69225c89820e0dc62e8def7fc19
Mike Mackintosh <m@zyp.io>
2022-07-23 01:51:19 -0700

    update some rgb toggling

--- qmk/duppster/keymap.c
+++ qmk/duppster/keymap.c
@@ -128,88 +128,88 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           }
           return false;
 
-    case U_T_AUTO:
+      case U_T_AUTO:
           if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
           TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
           }
           return false;
-    case U_T_AGCR:
-    if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
-    TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
-    }
-    return false;
-    case DBG_TOG:
-    if (record->event.pressed) {
-    TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
-    }
-    return false;
-    case DBG_MTRX:
-    if (record->event.pressed) {
-    TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
-    }
-    return false;
-    case DBG_KBD:
-    if (record->event.pressed) {
-    TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
-    }
-    return false;
-    case DBG_MOU:
-    if (record->event.pressed) {
-    TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
-    }
-    return false;
-    case MD_BOOT:
-    if (record->event.pressed) {
-    key_timer = timer_read32();
-    } else {
-    if (timer_elapsed32(key_timer) >= 500) {
-    reset_keyboard();
-    }
-    }
-    return false;
+      case U_T_AGCR:
+          if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+              TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+          }
+          return false;
+      case DBG_TOG:
+          if (record->event.pressed) {
+              TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+          }
+          return false;
+      case DBG_MTRX:
+          if (record->event.pressed) {
+              TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+          }
+          return false;
+      case DBG_KBD:
+          if (record->event.pressed) {
+              TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+          }
+          return false;
+      case DBG_MOU:
+          if (record->event.pressed) {
+              TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+          }
+          return false;
+      case MD_BOOT:
+          if (record->event.pressed) {
+              key_timer = timer_read32();
+          } else {
+              if (timer_elapsed32(key_timer) >= 500) {
+                  reset_keyboard();
+              }
+          }
+          return false;
 
-    case RGB_TOG:
-    if (record->event.pressed) {
-    switch (rgb_matrix_get_flags()) {
-    case LED_FLAG_ALL: {
-    rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
-    rgb_matrix_set_color_all(0, 0, 0);
-    }
-    break;
-    case LED_FLAG_KEYLIGHT: {
-    rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
-    rgb_matrix_set_color_all(0, 0, 0);
-    }
-    break;
-    case LED_FLAG_UNDERGLOW: {
-    rgb_matrix_set_flags(LED_FLAG_NONE);
-    rgb_matrix_disable_noeeprom();
-    }
-    break;
-    default: {
-    rgb_matrix_set_flags(LED_FLAG_ALL);
-    rgb_matrix_enable_noeeprom();
-    }
-    break;
-    }
-    }
-    break;
+      case RGB_TOG:
+          if (record->event.pressed) {
+            switch (rgb_matrix_get_flags()) {
+              case LED_FLAG_ALL: {
+                  rgb_matrix_set_flags(LED_FLAG_KEYLIGHT);
+                  rgb_matrix_set_color_all(0, 0, 0);
+                }
+                break;
+              case LED_FLAG_KEYLIGHT: {
+                  rgb_matrix_set_flags(LED_FLAG_UNDERGLOW);
+                  rgb_matrix_set_color_all(0, 0, 0);
+                }
+                break;
+              case LED_FLAG_UNDERGLOW: {
+                  rgb_matrix_set_flags(LED_FLAG_NONE);
+                  rgb_matrix_disable_noeeprom();
+                }
+                break;
+              default: {
+                  rgb_matrix_set_flags(LED_FLAG_ALL);
+                  rgb_matrix_enable_noeeprom();
+                }
+                break;
+            }
+          }
+          return false;
 
-    case G_INIT ... Z_LS:
-        if (record->event.pressed) {
-            send_string_with_delay(cmds[keycode - G_INIT], 1);
-        }
+      case G_INIT ... Z_LS:
+          if (record->event.pressed) {
+              send_string_with_delay(cmds[keycode - G_INIT], 1);
+          }
 
-        break;
+          break;
 
-    case E_PIN ... E_EDB:
-        if (record->event.pressed) {
-            send_string_with_delay(cmds[keycode - G_INIT], 1);
-            register_code(KC_ENT);
-            unregister_code(KC_ENT);
-        }
+      case E_PIN ... E_EDB:
+          if (record->event.pressed) {
+              send_string_with_delay(cmds[keycode - G_INIT], 1);
+              register_code(KC_ENT);
+              unregister_code(KC_ENT);
+          }
 
-        break;
+          break;
     }
 
     return true; //Process all other keycodes normally
