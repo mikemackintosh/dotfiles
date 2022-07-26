664cd645abcb9652043229c86019f60952e0e9c1
Mike Mackintosh <m@zyp.io>
2022-07-25 20:37:08 -0700

    update secret pin and phrase defaults

--- qmk/duppster/keymap.c
+++ qmk/duppster/keymap.c
@@ -1,6 +1,14 @@
 #include "keymap.h"
+
 #if __has_include("secrets.h")
 # include "secrets.h"
+#endif
+
+#ifndef SECRET_PIN
+  #define SECRET_PIN "ok"
+#endif
+#ifndef SECRET_PHARSE
+  #define SECRET_PHARSE "ok"
 #endif
 
 keymap_config_t keymap_config;
@@ -12,13 +20,6 @@ bool is_space_cadet_shift_active = false; //
 #endif
 #ifndef CLOSEPAREN_KEY
   #define CLOSEPAREN_KEY KC_0
-#endif
-
-#ifndef SECRET_PIN
-  #define SECRET_PIN "ok"
-#endif
-#ifndef SECRET_PHARSE
-  #define SECRET_PHARSE "ok"
 #endif
 
 static const char * cmds[] = {
@@ -41,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
   [1] = LAYOUT_65_ansi_blocker(
-    KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
     _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
     KC_LSCR, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, Z_LS,    _______, _______,          _______, KC_BRIU, \
     _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, LAG_SWP,   E_PIN,   E_EDB,          KC_VOLU, KC_BRID, \
