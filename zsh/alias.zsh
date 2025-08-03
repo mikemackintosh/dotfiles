3755f58211bbe34fdebf421f0b5dd7ceab61c4bd
Duppster <m@zyp.io>
2025-08-03 01:40:17 -0400

    oops

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -10,4 +10,23 @@ alias sudo='sudo -E'
 alias gs='git status'
 alias gl='git log --oneline --graph --decorate'
 alias gp='git pull'
-alias gcm='git commit -m "$@"'
+
+function gb {
+  if [[ ! -z "$1" ]]; then
+    git switch "$*"
+    return 0
+  fi
+
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  echo -e "\033[38;5;39mCurrent branch: $branch\033[0m"
+}
+
+function gcm {
+  local msg="$*"
+  echo $msg | grep -qE '^\s*$' && {
+    echo -e "\033[38;5;195mCommit message cannot be empty!\033[0m"
+    return 1
+  }
+
+  git commit -m "$msg"
+}
